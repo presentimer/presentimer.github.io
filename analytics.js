61010fcee46307550efbed165dc7b046007bd708
Takuto NAKAMURA (Kyome) <takuto-nakamura@cybozu.co.jp>
2024-02-09 16:46:36 +0900

    google analyticsのコードを埋め込み

---
+++ analytics.js
@@ -0,0 +1,9 @@
+document.write(
+  '<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJR2R07CH9"></script>'
+);
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-LJR2R07CH9");
